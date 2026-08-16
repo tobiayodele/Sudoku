a52238b56b27b8c7ec82b6bde8f32b808014cce5
ttobi <tobi.ayodele@hotmail.com>
2026-08-16 15:01:29 +0100

    Completed full 1000 trial run, adjusted sudoku experiment to take into account generation, removal, validation and total time.

--- src/sudoku/SudokuExperiment.java
+++ src/sudoku/SudokuExperiment.java
@@ -5,28 +5,49 @@ import java.io.IOException;
 
 public class SudokuExperiment {
     private String runTrial(int numberOfMissingCells, int trial){
+        long trialStart = System.nanoTime();
+
         SudokuGenerator generator = new SudokuGenerator();
+        long generationStart = System.nanoTime();
         SudokuBoard board = generator.generate();
+        long generationEnd = System.nanoTime();
+        long generationTime = generationEnd - generationStart;
+
+        long removalStart = System.nanoTime();
         SudokuBoard puzzle = generator.removeCells(board,numberOfMissingCells);
+        long removalEnd = System.nanoTime();
+        long removalTime = removalEnd- removalStart;
+
         CellValidator validator = new CellValidator();
         long solutionStart = System.nanoTime();
         int solutionCount = generator.solutionCounter(puzzle,validator);
         long solutionEnd = System.nanoTime();
         long solutionTime = solutionEnd - solutionStart;
 
-        return (numberOfMissingCells + "," + trial + "," + solutionCount + "," + solutionTime + "\n");
+        long trialEnd = System.nanoTime();
+        long totalTime = trialEnd - trialStart;
+
+        return (numberOfMissingCells + "," + trial + "," + solutionCount + "," + generationTime +
+                "," + removalTime + "," + solutionTime + "," + totalTime +  "\n");
     }
 
     static void main(String[] args) throws  IOException{
+        long experimentStart = System.nanoTime();
         SudokuExperiment experiment = new SudokuExperiment();
         FileWriter writer = new FileWriter("sudoku_experiment_1000.csv");
-        writer.write("missing_cells,trial,solution_count,solution_time_ns\n");
+        writer.write("missing_cells,trial,solution_count,generation_time_ns,removal_time_ns,solution_time_ns,total_time_ns\n");
         for (int i =0; i < 65; i++){
             for (int trial =1; trial <= 1000; trial ++){
                 writer.write(experiment.runTrial(i,trial));
+
             }
+            System.out.println("Number: " + i + " trial completed.");
         }
         writer.close();
+        long experimentEnd = System.nanoTime();
+        long experimentTIme = experimentEnd - experimentStart;
+        double experimentTimeSeconds = experimentTIme / 1_000_000_000.0;
+        System.out.println(experimentTimeSeconds + " Seconds");
     }
 
 }
