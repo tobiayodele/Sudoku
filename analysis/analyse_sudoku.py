05a0cd0b03e665b32884de70bfc9039817e579b6
ttobi <tobi.ayodele@hotmail.com>
2026-08-17 15:47:16 +0100

    create table containing processed results

--- analysis/analyse_sudoku.py
+++ analysis/analyse_sudoku.py
@@ -2,7 +2,7 @@ import  pandas as pd
 import matplotlib.pyplot as plt
 import statistics
 
-data = pd.read_csv("sudoku_experiment_1000.csv")
+data = pd.read_csv("data/sudoku_experiment_1000.csv")
 
 missing_cells =[]
 mean_times = []
@@ -61,3 +61,36 @@ plt.yscale("log")
 
 plt.title("Relationship between Missing Cells and Runtiem")
 plt.savefig("runtime_relationship.png")
+
+
+data = {
+    "Missing Cells" : missing_cells,
+    "Percentage for Unique Solution" : percentage_chance_of_unique_solution,
+    "Median Runtime(ms)" : median_times,
+    "Mean Runtime(ms)" : mean_times
+}
+
+table = pd.DataFrame(data)
+table.to_excel("sudoku_results.xlsx", index=False)
+
+table["Missing Cells"]= table["Missing Cells"].astype(str)
+table["Percentage for Unique Solution"] = table["Percentage for Unique Solution"].round(2)
+table["Median Runtime(ms)"] = table["Median Runtime(ms)"].round(3)
+table["Mean Runtime(ms)"] = table["Mean Runtime(ms)"].round(3)
+
+fig, ax = plt.subplots(figsize=(10, 12))
+
+ax.axis("off")
+
+table_plot = ax.table(
+    cellText=table.values,
+    colLabels=table.columns,
+    loc="center",
+    cellLoc="center"
+)
+
+table_plot.auto_set_font_size(False)
+table_plot.set_fontsize(10)
+table_plot.scale(1.2, 1.5)
+
+plt.savefig("sudoku_results.png", bbox_inches="tight", dpi=300)
