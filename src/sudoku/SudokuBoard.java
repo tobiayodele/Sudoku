48c471b5585e7e13b95de669e6cba44e1132b877
ttobi <tobi.ayodele@hotmail.com>
2026-08-17 13:26:40 +0100

    Updated board print function.

--- src/sudoku/SudokuBoard.java
+++ src/sudoku/SudokuBoard.java
@@ -7,19 +7,32 @@ public class SudokuBoard {
         board =  new int [9][9];
     }
 
-     void print (){
-        for (int [] row : board){
-            for (int cell : row){
-                if (cell == 0){
+    public void print (){
+        String reset = "\u001B[0m";
+        String red = "\u001B[31m";
+
+        System.out.println(red + "____________|_____________|____________" + reset);
+        for (int row = 0; row < 9 ; row ++ ){
+            if (row % 3 == 0 && row != 0 ){
+                System.out.println(red +"____________|_____________|____________" + reset);
+
+            }
+            for (int column=0; column < 9; column ++){
+                if (column % 3 == 0 && column!= 0){
+                    System.out.print(red + "| " + reset);
+                }
+                if (this.board[row][column]==0){
+
                     System.out.print("[" + " " + "]"+ " ");
                 }
                 else{
-                    System.out.print("[" + cell + "]"+ " ");
+                    System.out.print("[" + this.board[row][column] + "]"+ " ");
                 }
-
             }
             System.out.println();
         }
+        System.out.println(red + "____________|_____________|____________" + reset);
+        System.out.println(red + "            |             |            " + reset);
     }
 
     public boolean isEmpty( int row,int  column){
