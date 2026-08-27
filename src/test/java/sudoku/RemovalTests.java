8f3cc3d000dde731aee13cfe974175c9329ffd08
ttobi <tobi.ayodele@hotmail.com>
2026-08-27 14:35:29 +0100

    Added iterative and recursive removal unit tests

--- src/test/java/sudoku/RemovalTests.java
+++ src/test/java/sudoku/RemovalTests.java
@@ -49,7 +49,7 @@ public class RemovalTests {
     public void correctNumberOfRecursiveRemovedCells(){
         SudokuGenerator generator = new SudokuGenerator();
         SudokuBoard board = generator.generate();
-        SudokuBoard removedCellsBoard = generator.iterativeRemoveCells(board, 50);
+        SudokuBoard removedCellsBoard = generator.recursiveRemoveCells(board, 50);
         // check only 50 cells have been removed
         int count = 0;
         for(int [] row : removedCellsBoard.getBoard()){
@@ -66,7 +66,7 @@ public class RemovalTests {
     public void noOtherRecursiveCellsAffected(){
         SudokuGenerator generator = new SudokuGenerator();
         SudokuBoard board = generator.generate();
-        SudokuBoard removedCellsBoard = generator.iterativeRemoveCells(board, 20);
+        SudokuBoard removedCellsBoard = generator.recursiveRemoveCells(board, 20);
         boolean flag = true;
         for (int i = 0; i < 9; i++){
             for (int j=0; j<9; j++){
@@ -87,6 +87,53 @@ public class RemovalTests {
 
     @Test
     public void recursiveRemovalUniqueSolution(){
+        SudokuGenerator generator = new SudokuGenerator();
+        SudokuBoard board = generator.generate();
+        CellValidator validator = new CellValidator();
+        SudokuBoard removedCellsBoard = generator.recursiveRemoveCells(board, 50);
+        assertEquals(1, generator.solutionCounter(removedCellsBoard, validator));
+    }
+
+    @Test
+    public void correctNumberOfIterativeRemovedCells(){
+        SudokuGenerator generator = new SudokuGenerator();
+        SudokuBoard board = generator.generate();
+        SudokuBoard removedCellsBoard = generator.iterativeRemoveCells(board, 20);
+        // check only 20 cells have been removed
+        int count = 0;
+        for(int [] row : removedCellsBoard.getBoard()){
+            for (int cell : row){
+                if(cell == 0){
+                    count ++;
+                }
+            }
+        }
+        assertEquals(20, count);
+    }
+
+    @Test
+    public void noOtherIterativeCellsAffected(){
+        SudokuGenerator generator = new SudokuGenerator();
+        SudokuBoard board = generator.generate();
+        SudokuBoard removedCellsBoard = generator.iterativeRemoveCells(board, 20);
+        boolean flag = true;
+        for (int i = 0; i < 9; i++){
+            for (int j=0; j<9; j++){
+                if (removedCellsBoard.getBoard()[i][j]==0){
+                    continue;
+                }
+                else{
+                    if(!(removedCellsBoard.getBoard()[i][j] == board.getBoard()[i][j])){
+                        flag = false;
+                    }
+                }
+            }
+        }
+        assertTrue(flag);
+    }
+
+    @Test
+    public void iterativeRemovalUniqueSolution(){
         SudokuGenerator generator = new SudokuGenerator();
         SudokuBoard board = generator.generate();
         CellValidator validator = new CellValidator();
