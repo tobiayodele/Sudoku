90d1f79b0ab7569c2f081b3d5239e2f1a9e6dc29
ttobi <tobi.ayodele@hotmail.com>
2026-09-03 00:09:39 +0100

    edited tests to include junit 4 assertions

--- src/test/java/sudoku/CellValidatorTests.java
+++ src/test/java/sudoku/CellValidatorTests.java
@@ -1,10 +1,7 @@
 package sudoku;
 
 import org.junit.Test;
-
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class CellValidatorTests {
 
--- src/test/java/sudoku/RemovalTests.java
+++ src/test/java/sudoku/RemovalTests.java
@@ -1,10 +1,7 @@
 package sudoku;
 
 import org.junit.Test;
-
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class RemovalTests {
     @Test
