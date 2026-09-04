ca21287e5f74763302f546e533a1ca075f1dea5e
ttobi <tobi.ayodele@hotmail.com>
2026-09-04 10:47:19 +0100

    rm: multipleSolutions test: iterative removal returns unique or null so can't test for this

--- src/test/java/sudoku/SolutionCounterTests.java
+++ src/test/java/sudoku/SolutionCounterTests.java
@@ -24,17 +24,7 @@ public class SolutionCounterTests {
         assertEquals(1, generator.solutionCounter(board, validator));
     }
 
-    @Test
-    public void multiplePossibleSolutions(){
-        // A puzzle needs at least 17 filled in cells to have a unique solution
-        // (McGuire, Tugemann & Civario, 2014)
-        // 81-16 = 65 hence taking out 65 cells results in a guaranteed puzzle
-        // that does not have a unique solution.
-        SudokuGenerator generator = new SudokuGenerator();
-        SudokuBoard board = generator.generate();
-        board = generator.randomRemoveCells(board, 65);
-        CellValidator validator = new CellValidator();
-        assertEquals(2, generator.solutionCounter(board,validator) );
-    }
+    // removed multiple solutions check. generator.iterativeRemoveCells will always return a unique solution
+    // or null.
 
 }
