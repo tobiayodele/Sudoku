55c288bd9f0511ce13e686014190c73b24f5253d
ttobi <tobi.ayodele@hotmail.com>
2026-09-06 21:27:14 +0100

    added comments

--- src/test/java/sudoku/SudokuSolverTests.java
+++ src/test/java/sudoku/SudokuSolverTests.java
@@ -11,10 +11,13 @@ public class SudokuSolverTests {
     public void returnsCompletedSudoku(){
         SudokuGenerator generator = new SudokuGenerator();
         SudokuBoard board = generator.generate();
+        // At 50 missing cells, will always result in unique Sudoku
+        // see experiments branch & report
         SudokuBoard puzzle = generator.iterativeRemoveCells(board, 50);
         SudokuSolver solver = new SudokuSolver();
         SudokuBoard solution = solver.solve(puzzle);
         // puzzle generated from board so solution should be the same as board
         assertArrayEquals(solution.getBoard(),board.getBoard());
     }
+
 }
