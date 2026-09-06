4ba2456e74379ebaebe524b3c67ae38642adabaa
ttobi <tobi.ayodele@hotmail.com>
2026-09-06 14:27:07 +0100

    Added startup screen and solver main method

--- src/main/java/sudoku/Main.java
+++ src/main/java/sudoku/Main.java
@@ -1,8 +1,39 @@
 package sudoku;
 
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        SudokuGame game = new SudokuGame();
-        game.startGame();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("===== Sudoku =====\n" +
+                "1. Play Sudoku\n" +
+                "2. Solve Sudoku\n" +
+                "3. Exit\n" + "\n" +
+                "Choose an option:");
+        String mode;
+        while (true){
+
+            mode = scanner.nextLine();
+            if (!(mode.equals("1") || mode.equals("2") || mode.equals("3"))){
+                System.out.println("Invalid Input");
+                System.out.println("Enter 1, 2 or 3: ");
+            }
+            else{break;}
+        }
+
+        switch (mode){
+            case "1":
+                SudokuGame game = new SudokuGame();
+                game.startGame();
+                break;
+            case "2":
+                SudokuSolver solver = new SudokuSolver();
+                solver.solveSudoku();
+                break;
+            case "3":
+                System.exit(0);
+        }
+
     }
 }
--- src/main/java/sudoku/SudokuSolver.java
+++ src/main/java/sudoku/SudokuSolver.java
@@ -1,5 +1,7 @@
 package sudoku;
 
+import java.util.Scanner;
+
 public class SudokuSolver {
      SudokuBoard solve(SudokuBoard puzzle){
         CellValidator validator = new CellValidator();
@@ -29,5 +31,35 @@ public class SudokuSolver {
         }
         //completed sudoku by this point
         return puzzle;
+    }
+
+    SudokuBoard enterBoard(){
+         Scanner scanner = new Scanner(System.in);
+         SudokuBoard board = new SudokuBoard();
+         for (int i =0; i <81; i++){
+             int row = i / 9;
+             int column = i % 9;
+             System.out.println("Enter number for box (" + row + "," + column + "): (Enter 0 for empty)");
+             int value = scanner.nextInt();
+             board.setCell(row,column,value);
+
+         }
+         return board;
+    }
+
+
+    void solveSudoku(){
+         SudokuBoard board = enterBoard();
+         SudokuGenerator generator = new SudokuGenerator();
+         CellValidator validator = new CellValidator();
+         int solutions = generator.solutionCounter(board, validator);
+         if (solutions > 1){
+             System.out.println("Not uniquely solvable");
+         }
+         else{
+             SudokuBoard solution = solve(board);
+             System.out.println("Success!");
+             solution.print();
+         }
     }
 }
